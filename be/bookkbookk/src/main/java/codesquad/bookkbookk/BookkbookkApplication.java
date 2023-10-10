ec23a3deda6a90386fda20e551685ebb972bf2f5
swinb <tmdqja0705@naver.com>
2023-10-10 15:58:38 +0900

    chore: BE 스프링 부트 프로젝트 초기 환경 설정

---
+++ be/bookkbookk/src/main/java/codesquad/bookkbookk/BookkbookkApplication.java
@@ -0,0 +1,13 @@
+package codesquad.bookkbookk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkbookkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkbookkApplication.class, args);
+	}
+
+}
