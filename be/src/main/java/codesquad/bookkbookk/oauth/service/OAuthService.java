d1110706ea458e401ab16cc621fcdbde75f61065
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-16 23:10:12 +0900

    feat: 로그인 기능 구현 #5
    - 이메일로 db에 멤버가 있는지 확인하고 멤버가 있으면 멤버를 불러오고 멤버가 없으면 멤버를 만들고 저장합니다.
    - 멤버의 아이디로 Jwt를 생성하여 응답으로 넘겨줍니다.

--- be/src/main/java/codesquad/bookkbookk/oauth/service/OAuthService.java
+++ be/src/main/java/codesquad/bookkbookk/oauth/service/OAuthService.java
@@ -7,22 +7,55 @@ import java.util.Map;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import codesquad.bookkbookk.oauth.OAuthProvider;
+import codesquad.bookkbookk.jwt.Jwt;
+import codesquad.bookkbookk.jwt.JwtProvider;
+import codesquad.bookkbookk.member.data.entity.Member;
+import codesquad.bookkbookk.member.repository.MemberRepository;
+import codesquad.bookkbookk.oauth.data.provider.OAuthProvider;
+import codesquad.bookkbookk.oauth.data.dto.LoginResponse;
 import codesquad.bookkbookk.oauth.data.dto.OAuthTokenResponse;
+import codesquad.bookkbookk.oauth.data.dto.LoginRequest;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class OAuthService {
 
-    public String requestOAuthToken(OAuthProvider.Property oAuthProperty, String authCode) {
+    private final MemberRepository memberRepository;
+    private final OAuthProvider oAuthProvider;
+    private final JwtProvider jwtProvider;
+
+    @Transactional
+    public LoginResponse login(String authCode, String providerName) {
+        OAuthProvider.Property oAuthProperty = oAuthProvider.getProperties().get(providerName);
+        String oAuthToken = requestOAuthToken(oAuthProperty, authCode);
+        Map<String, Object> oAuthUserInfos = requestOAuthUserInfos(oAuthProperty, oAuthToken);
+        LoginRequest loginRequest = LoginRequest.of(providerName, oAuthUserInfos);
+
+        boolean doesMemberExist = memberRepository.existsByEmail(loginRequest.getEmail());
+        Member loginMember = getLoginMember(loginRequest, doesMemberExist);
+        Jwt jwt = jwtProvider.createJwt(loginMember.getId());
+
+        return LoginResponse.of(jwt, doesMemberExist);
+    }
+
+    private Member getLoginMember(LoginRequest loginRequest, boolean doesMemberExist) {
+        if (doesMemberExist) {
+            return memberRepository.findByEmail(loginRequest.getEmail())
+                    .orElseThrow(RuntimeException::new);
+        }
+        return memberRepository.save(Member.from(loginRequest));
+    }
+
+    private String requestOAuthToken(OAuthProvider.Property oAuthProperty, String authCode) {
         return WebClient.create()
                 .post()
-                .uri(oAuthProperty.getTokenRequestUri())
+                .uri(oAuthProperty.getTokenRequestUrl())
                 .headers(header -> {
                     header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
                     header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
@@ -35,24 +68,24 @@ public class OAuthService {
                 .block();
     }
 
-    private MultiValueMap<String, String> createTokenRequestBody(OAuthProvider.Property property,
+    private MultiValueMap<String, String> createTokenRequestBody(OAuthProvider.Property oAuthProperty,
                                                                  String authCode) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
 
-        formData.add("client_id", property.getClientId());
-        formData.add("client_secret", property.getClientSecret());
-        formData.add("redirect_uri", property.getRedirectUri());
+        formData.add("client_id", oAuthProperty.getClientId());
+        formData.add("client_secret", oAuthProperty.getClientSecret());
+        formData.add("redirect_uri", oAuthProperty.getRedirectUrl());
         formData.add("code", authCode);
         formData.add("grant_type", "authorization_code");
         return formData;
     }
 
-    public Map<String, Object> requestOAuthUserInfo(OAuthProvider.Property oAuthDetail,
-                                                    String tokenResponse) {
+    private Map<String, Object> requestOAuthUserInfos(OAuthProvider.Property oAuthProperty,
+                                                    String oAuthToken) {
         return WebClient.create()
                 .get()
-                .uri(oAuthDetail.getUserInfoRequestUri())
-                .headers(header -> header.setBearerAuth(tokenResponse))
+                .uri(oAuthProperty.getUserInfoRequestUrl())
+                .headers(header -> header.setBearerAuth(oAuthToken))
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
                 })
