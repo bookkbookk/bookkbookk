46d3f8d8f76b5a0518ceac32170397357d2ad591
n-aa-g <wlsdnjs0124@gmail.com>
2023-10-16 23:29:01 +0900

    chore: 주석 제거, 쓰지 않는 의존성 제거, #5

--- be/src/main/java/codesquad/bookkbookk/auth/controller/AuthController.java
+++ be/src/main/java/codesquad/bookkbookk/auth/controller/AuthController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import codesquad.bookkbookk.jwt.JwtProvider;
-import codesquad.bookkbookk.auth.data.dto.OAuthCode;
 import codesquad.bookkbookk.auth.data.dto.LoginResponse;
+import codesquad.bookkbookk.auth.data.dto.OAuthCode;
 import codesquad.bookkbookk.auth.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 
@@ -19,9 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final OAuthService oAuthService;
-    private final JwtProvider jwtFactory;
 
-    //기능 동작 테스트용 (@RequestParam을 @RequestBody로 변경할 것)
     @PostMapping("/login/{providerName}")
     public ResponseEntity<LoginResponse> login(@RequestBody OAuthCode oAuthCode,
                                                @PathVariable String providerName) {
