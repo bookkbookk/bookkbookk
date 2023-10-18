7fa085786ce59a8a3a5c8f267a0dd7ae4db73d47
nag0124 <wlsdnjs0124@gmail.com>
2023-10-18 12:13:17 +0900

    Feat: Jwt 필터 구현 #14

--- be/src/main/java/codesquad/bookkbookk/config/FilterConfig.java
+++ be/src/main/java/codesquad/bookkbookk/config/FilterConfig.java
@@ -6,16 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import codesquad.bookkbookk.filter.CorsFilter;
+import codesquad.bookkbookk.filter.JwtFilter;
+import codesquad.bookkbookk.jwt.JwtProvider;
 
 @Configuration
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter(@Value("${cors.origin}") String allowedOrigin) {
+    public FilterRegistrationBean<CorsFilter> corsFilter(@Value("${cors.allowed-origin}") String allowedOrigin) {
         FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>();
 
         filterRegistrationBean.setFilter(new CorsFilter(allowedOrigin));
         filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter(JwtProvider provider) {
+        FilterRegistrationBean<JwtFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new JwtFilter(provider));
+        filterRegistrationBean.setOrder(2);
         return filterRegistrationBean;
     }
 
