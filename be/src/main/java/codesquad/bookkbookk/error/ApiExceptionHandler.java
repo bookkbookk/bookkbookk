399e58a8a814e865d4d0adceae67227f41e1793c
nag0124 <wlsdnjs0124@gmail.com>
2023-10-18 16:16:41 +0900

    Feat: ErrorResponse 생성 #14
    - ApiResponse를 바로 ResponseEntity에 주입할 경우, 필요없는 정보들이 들어갑니다. 이를 필터링 하기 위해 ErrorResponse를 만들었습니다.

--- be/src/main/java/codesquad/bookkbookk/error/ApiExceptionHandler.java
+++ be/src/main/java/codesquad/bookkbookk/error/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import codesquad.bookkbookk.error.exception.ApiException;
+import codesquad.bookkbookk.error.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,10 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ApiException> handleAddressNotFoundException(ApiException exception) {
+    public ResponseEntity<ErrorResponse> handleAddressNotFoundException(ApiException exception) {
         log.warn("ApiException handling : {}", exception.toString());
+        ErrorResponse response = ErrorResponse.from(exception);
 
-        return ResponseEntity.status(exception.getStatus()).body(exception);
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
     }
 }
 
