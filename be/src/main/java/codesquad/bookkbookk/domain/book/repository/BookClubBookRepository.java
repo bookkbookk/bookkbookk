8ec8dc1f5ee13b06db5c89362481f665cab5d111
swinb <tmdqja0705@naver.com>
2023-10-19 17:05:02 +0900

    feat: BookClubBook 엔티티 구현 #15

---
+++ be/src/main/java/codesquad/bookkbookk/domain/book/repository/BookClubBookRepository.java
@@ -0,0 +1,9 @@
+package codesquad.bookkbookk.domain.book.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import codesquad.bookkbookk.domain.book.data.entity.BookClubBook;
+
+public interface BookClubBookRepository extends JpaRepository<BookClubBook, Long> {
+
+}
