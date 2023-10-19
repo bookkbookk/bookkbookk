88ae7df19a6bfad7e67b0448b4099ef018cffbb7
swinb <tmdqja0705@naver.com>
2023-10-19 17:06:08 +0900

    feat: 북클럽에 권한이 없는 사용자 에러 코드 구현

---
+++ be/src/main/java/codesquad/bookkbookk/common/error/exception/AccessDeniedBookClubError.java
@@ -0,0 +1,11 @@
+package codesquad.bookkbookk.common.error.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class AccessDeniedBookClubError extends ApiException {
+
+    public AccessDeniedBookClubError() {
+        super(HttpStatus.UNAUTHORIZED, "해당 북클럽에 권한이 없습니다..");
+    }
+
+}
