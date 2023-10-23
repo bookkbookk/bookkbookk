2ecdb043f8e2b2a2b26ba9ba5862a46ae179faf8
swinb <tmdqja0705@naver.com>
2023-10-23 14:53:46 +0900

    refactor: BookClubBook 매핑 테이블 삭제 #41

--- be/src/main/java/codesquad/bookkbookk/domain/book/repository/BookClubBookRepository.java
+++
@@ -1,9 +0,0 @@
-package codesquad.bookkbookk.domain.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import codesquad.bookkbookk.domain.book.data.entity.BookClubBook;
-
-public interface BookClubBookRepository extends JpaRepository<BookClubBook, Long> {
-
-}
