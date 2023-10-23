5efbc432227aeb91fad6b741f244185180d4a1bb
nag0124 <wlsdnjs0124@gmail.com>
2023-10-23 23:13:57 +0900

    feat: TokenArgumentResolver 구현 #33
    - httpServletRequest에서 token을 추출하는 기능을 구현했습니다.

---
+++ be/src/main/java/codesquad/bookkbookk/common/resolver/TokenArgumentResolver.java
@@ -0,0 +1,36 @@
+package codesquad.bookkbookk.common.resolver;
+
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import codesquad.bookkbookk.common.jwt.JwtProvider;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class TokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(MemberId.class) && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public String resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  @NonNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        return jwtProvider.getToken(Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class)));
+    }
+
+}
