4b5480ba1d24f19aefedc7e5b5544d20e5dff120
swinb <103398897+swinb@users.noreply.github.com>
2023-10-24 15:16:53 +0900

    [BE] 나의 책 목록 조회 기능 구현 (#46)
    * feat: MemberBook 엔티티 구현 #45
    
    * fix: 엔티티 이름 대문자 오타 수정 #45
    
    * feat: 나의 책 목록 조회 기능 구현 #45
    
    * feat: 나의 책 목록 조회 기능 테스트 성공 케이스 구현 #45

--- be/src/main/java/codesquad/bookkbookk/domain/book/controller/BookController.java
+++ be/src/main/java/codesquad/bookkbookk/domain/book/controller/BookController.java
@@ -1,14 +1,17 @@
 package codesquad.bookkbookk.domain.book.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import codesquad.bookkbookk.common.resolver.MemberId;
 import codesquad.bookkbookk.domain.book.data.dto.CreateBookRequest;
 import codesquad.bookkbookk.domain.book.data.dto.CreateBookResponse;
+import codesquad.bookkbookk.domain.book.data.dto.ReadBookResponse;
 import codesquad.bookkbookk.domain.book.service.BookService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,15 @@ public class BookController {
     @PostMapping
     public ResponseEntity<CreateBookResponse> createBook(@MemberId Long memberId, @RequestBody CreateBookRequest request) {
         CreateBookResponse response = bookService.createBook(memberId, request);
+
+        return ResponseEntity.ok()
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<ReadBookResponse> readBooks(@MemberId Long memberId, @RequestParam Integer page,
+                                                      @RequestParam Integer size) {
+        ReadBookResponse response = bookService.readBooks(memberId, page, size);
 
         return ResponseEntity.ok()
                 .body(response);
--- be/src/main/java/codesquad/bookkbookk/domain/book/service/BookService.java
+++ be/src/main/java/codesquad/bookkbookk/domain/book/service/BookService.java
@@ -1,13 +1,24 @@
 package codesquad.bookkbookk.domain.book.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import codesquad.bookkbookk.common.error.exception.MemberNotFoundException;
 import codesquad.bookkbookk.common.error.exception.MemberNotInBookClubException;
 import codesquad.bookkbookk.domain.book.data.dto.CreateBookRequest;
 import codesquad.bookkbookk.domain.book.data.dto.CreateBookResponse;
+import codesquad.bookkbookk.domain.book.data.dto.ReadBookResponse;
 import codesquad.bookkbookk.domain.book.data.entity.Book;
+import codesquad.bookkbookk.domain.book.data.entity.MemberBook;
 import codesquad.bookkbookk.domain.book.repository.BookRepository;
+import codesquad.bookkbookk.domain.book.repository.MemberBookRepository;
 import codesquad.bookkbookk.domain.bookclub.repository.MemberBookClubRepository;
+import codesquad.bookkbookk.domain.member.data.entity.Member;
+import codesquad.bookkbookk.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +28,8 @@ public class BookService {
 
     private final BookRepository bookRepository;
     private final MemberBookClubRepository memberBookClubRepository;
+    private final MemberRepository memberRepository;
+    private final MemberBookRepository memberBookRepository;
 
     public CreateBookResponse createBook(Long memberId, CreateBookRequest request) {
         if (!memberBookClubRepository.existsByMemberIdAndBookClubId(memberId, request.getBookClubId())) {
@@ -26,7 +39,23 @@ public class BookService {
         Book book = Book.from(request);
         bookRepository.save(book);
 
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
+        MemberBook memberBook = new MemberBook(member, book);
+        memberBookRepository.save(memberBook);
+
         return new CreateBookResponse(book.getId());
+    }
+
+    public ReadBookResponse readBooks(Long memberId, Integer page, Integer size) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "title");
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+        Slice<Book> bookSlice = bookRepository.findBooksByMemberId(memberId, pageRequest);
+
+        List<Book> books = bookSlice.getContent();
+        Boolean hasNext = bookSlice.hasNext();
+
+        return new ReadBookResponse(books, hasNext);
     }
 
 }
