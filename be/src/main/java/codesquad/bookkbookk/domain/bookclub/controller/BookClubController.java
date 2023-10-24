9dffc70e0b7a39683d12cd44400ba70853219dfa
swinb <tmdqja0705@naver.com>
2023-10-24 16:34:20 +0900

    refactor: MemberId 추출 방식 개선된 버전으로 변경

--- be/src/main/java/codesquad/bookkbookk/domain/bookclub/controller/BookClubController.java
+++ be/src/main/java/codesquad/bookkbookk/domain/bookclub/controller/BookClubController.java
@@ -2,8 +2,6 @@ package codesquad.bookkbookk.domain.bookclub.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,11 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import codesquad.bookkbookk.common.resolver.MemberId;
 import codesquad.bookkbookk.domain.bookclub.data.dto.CreateBookClubRequest;
 import codesquad.bookkbookk.domain.bookclub.data.dto.CreateBookClubResponse;
 import codesquad.bookkbookk.domain.bookclub.data.dto.ReadBookClubResponse;
 import codesquad.bookkbookk.domain.bookclub.service.BookClubService;
-import codesquad.bookkbookk.common.jwt.JwtProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,38 +22,22 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/book-clubs")
 public class BookClubController {
 
-    private static final String BEARER_PREFIX = "Bearer ";
-
-    private final JwtProvider jwtProvider;
     private final BookClubService bookClubService;
 
     @PostMapping
-    public ResponseEntity<CreateBookClubResponse> createBookClub(HttpServletRequest httpServletRequest,
+    public ResponseEntity<CreateBookClubResponse> createBookClub(@MemberId Long memberId,
                                                                  @ModelAttribute CreateBookClubRequest request) {
-        String accessToken = extractAccessToken(httpServletRequest);
-        Long memberId = jwtProvider.extractMemberId(accessToken);
-
         CreateBookClubResponse response = bookClubService.createBookClub(memberId, request);
-
         return ResponseEntity.ok()
                 .body(response);
-
     }
 
     @GetMapping
-    public ResponseEntity<List<ReadBookClubResponse>> readBookClubs(HttpServletRequest httpServletRequest) {
-        String accessToken = extractAccessToken(httpServletRequest);
-        Long memberId = jwtProvider.extractMemberId(accessToken);
-
+    public ResponseEntity<List<ReadBookClubResponse>> readBookClubs(@MemberId Long memberId) {
         List<ReadBookClubResponse> response = bookClubService.readBookClubs(memberId);
 
         return ResponseEntity.ok()
                 .body(response);
-    }
-
-    private String extractAccessToken(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization");
-        return authorizationHeader.substring(BEARER_PREFIX.length());
     }
 
 }
