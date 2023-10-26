46e8d11fdada3b271b8d65a09ce078a1fc31755d
swinb <tmdqja0705@naver.com>
2023-10-26 12:24:52 +0900

    test: 토픽 U,D 성공케이스 테스트 코드 구현 #17

--- be/src/test/java/codesquad/bookkbookk/topic/integration/TopicTest.java
+++ be/src/test/java/codesquad/bookkbookk/topic/integration/TopicTest.java
@@ -8,10 +8,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 import codesquad.bookkbookk.IntegrationTest;
+import codesquad.bookkbookk.common.error.exception.TopicNotFoundException;
 import codesquad.bookkbookk.common.jwt.JwtProvider;
 import codesquad.bookkbookk.domain.member.data.entity.Member;
 import codesquad.bookkbookk.domain.member.repository.MemberRepository;
 import codesquad.bookkbookk.domain.topic.data.dto.CreateTopicRequest;
+import codesquad.bookkbookk.domain.topic.data.dto.UpdateTopicTitleRequest;
 import codesquad.bookkbookk.domain.topic.data.entity.Topic;
 import codesquad.bookkbookk.domain.topic.repository.TopicRepository;
 import codesquad.bookkbookk.util.TestDataFactory;
@@ -87,6 +89,66 @@ public class TopicTest extends IntegrationTest {
         SoftAssertions.assertSoftly(softAssertions -> {
             softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
             softAssertions.assertThat(response.jsonPath().getList("")).hasSize(2);
+        });
+    }
+
+    @DisplayName("성공적으로 토픽의 제목을 수정한다")
+    @Test
+    void updateTitle() {
+        //given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        Topic topic = TestDataFactory.createTopic(1L, "토픽");
+        topicRepository.save(topic);
+
+        UpdateTopicTitleRequest request = new UpdateTopicTitleRequest("updated title");
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                .when()
+                    .patch("/api/topics/"+ topic.getId())
+                .then().log().all()
+                    .extract();
+
+        //then
+        Topic result = topicRepository.findById(topic.getId()).orElseThrow(TopicNotFoundException::new);
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(result.getTitle()).isEqualTo("updated title");
+        });
+    }
+
+    @DisplayName("성공적으로 토픽을 삭제한다")
+    @Test
+    void deleteTopic() {
+        //given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        Topic topic = TestDataFactory.createTopic(1L, "토픽");
+        topicRepository.save(topic);
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .when()
+                .delete("/api/topics/"+ topic.getId())
+                .then().log().all()
+                .extract();
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(topicRepository.findById(topic.getId())).isEmpty();
         });
     }
 }
