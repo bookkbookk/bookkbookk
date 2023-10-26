c25d30cc29e04336c891ca903107bff348be0d74
swinb <tmdqja0705@naver.com>
2023-10-26 11:16:56 +0900

    feat: dto 생성자 생성 #17

--- be/src/main/java/codesquad/bookkbookk/domain/topic/data/dto/CreateTopicRequest.java
+++ be/src/main/java/codesquad/bookkbookk/domain/topic/data/dto/CreateTopicRequest.java
@@ -7,4 +7,10 @@ public class CreateTopicRequest {
 
     private Long chapterId;
     private String title;
+
+    public CreateTopicRequest(Long chapterId, String title) {
+        this.chapterId = chapterId;
+        this.title = title;
+    }
+
 }
