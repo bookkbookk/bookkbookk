ff516df32bfbe5b57b3eea3c9d57535ed632000d
nag0124 <wlsdnjs0124@gmail.com>
2023-10-26 16:06:35 +0900

    feat: request dto에 생성자 추가 #56
    - JPA가 쓰는 생성자 말고 코드상에서 쓸 생성자를 추가했습니다.

--- be/src/main/java/codesquad/bookkbookk/domain/gathering/data/dto/CreateGatheringRequest.java
+++ be/src/main/java/codesquad/bookkbookk/domain/gathering/data/dto/CreateGatheringRequest.java
@@ -3,10 +3,12 @@ package codesquad.bookkbookk.domain.gathering.data.dto;
 import java.time.LocalDateTime;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class CreateGatheringRequest {
 
