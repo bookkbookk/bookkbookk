1587192087352ff9efc9b93f1aee9fd6950b644d
nag0124 <wlsdnjs0124@gmail.com>
2023-10-31 21:32:33 +0900

    test: bookmark 생성 기능 테스트 작성 #84

---
+++ be/src/test/java/codesquad/bookkbookk/bookmark/integration/BookmarkTest.java
@@ -0,0 +1,86 @@
+package codesquad.bookkbookk.bookmark.integration;
+
+import java.util.Map;
+
+import org.assertj.core.api.SoftAssertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import codesquad.bookkbookk.IntegrationTest;
+import codesquad.bookkbookk.common.jwt.JwtProvider;
+import codesquad.bookkbookk.domain.book.data.entity.Book;
+import codesquad.bookkbookk.domain.book.repository.BookRepository;
+import codesquad.bookkbookk.domain.bookclub.data.entity.BookClub;
+import codesquad.bookkbookk.domain.bookclub.repository.BookClubRepository;
+import codesquad.bookkbookk.domain.chapter.data.entity.Chapter;
+import codesquad.bookkbookk.domain.chapter.repository.ChapterRepository;
+import codesquad.bookkbookk.domain.member.data.entity.Member;
+import codesquad.bookkbookk.domain.member.repository.MemberRepository;
+import codesquad.bookkbookk.domain.topic.data.entity.Topic;
+import codesquad.bookkbookk.domain.topic.repository.TopicRepository;
+import codesquad.bookkbookk.util.TestDataFactory;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class BookmarkTest extends IntegrationTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    BookClubRepository bookClubRepository;
+    @Autowired
+    BookRepository bookRepository;
+    @Autowired
+    ChapterRepository chapterRepository;
+    @Autowired
+    TopicRepository topicRepository;
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Test
+    @DisplayName("북마크를 생성한다.")
+    void createBookmark() {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        BookClub bookClub = TestDataFactory.createBookClub();
+        bookClubRepository.save(bookClub);
+
+        Book book = TestDataFactory.createBook1(bookClub);
+        bookRepository.save(book);
+
+        Chapter chapter = TestDataFactory.createChapter1(book);
+        chapterRepository.save(chapter);
+
+        Topic topic = TestDataFactory.createTopic1(chapter);
+        topicRepository.save(topic);
+
+        JSONObject requestBody = new JSONObject(Map.of("topicId", topic.getId(),
+                "title", "title",
+                "content", "content"));
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .when()
+                .post("api/bookmarks")
+                .then()
+                .extract();
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        });
+    }
+}
