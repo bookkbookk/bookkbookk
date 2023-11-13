0f4edfa51220ae15f8165f86d4670ac62e5448f9
Jisoo Yoo <youzysu@gmail.com>
2023-11-13 20:09:23 +0900

    UI: Sidebar active 상태 수정, 개발 중인 페이지 안내

--- fe/src/pages/Main.tsx
+++ fe/src/pages/Main.tsx
@@ -1,3 +1,5 @@
+import StatusIndicator from "@components/common/StatusIndicator/StatusIndicator";
+import { Box } from "@mui/material";
 import { Navigate } from "react-router-dom";
 import { ROUTE_PATH } from "routes/constants";
 
@@ -8,5 +10,12 @@ export default function Main() {
     return <Navigate to={`${ROUTE_PATH.join}/${invitationCode}`} replace />;
   }
 
-  return <div>메인메인</div>;
+  return (
+    <Box width="100%" height="100%">
+      <StatusIndicator
+        status="developing"
+        message={`메인 페이지는 열심히 개발하고 있어요! \n Coming soon!`}
+      />
+    </Box>
+  );
 }
