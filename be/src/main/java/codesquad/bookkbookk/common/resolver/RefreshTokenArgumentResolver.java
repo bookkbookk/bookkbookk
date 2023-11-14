16d7321e55e6dd5d17fb56b987f520f0b263973a
nag0124 <wlsdnjs0124@gmail.com>
2023-11-14 15:07:38 +0900

    feat: TokenArgumentResolver에서 cookie값을 가져오게 변경 #108
    - 클래스의 역할이 잘 드러나게 RefreshTokenArgumentResolver로 이름을 변경했습니다.
    - cookie가 없거나 cookie에 refreshToken이 없다면 예외를 던집니다.

--- be/src/main/java/codesquad/bookkbookk/common/resolver/RefreshTokenArgumentResolver.java
+++ be/src/main/java/codesquad/bookkbookk/common/resolver/RefreshTokenArgumentResolver.java
@@ -2,6 +2,7 @@ package codesquad.bookkbookk.common.resolver;
 
 import java.util.Objects;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.MethodParameter;
@@ -11,6 +12,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import codesquad.bookkbookk.common.error.exception.RefreshTokenNotFoundException;
 import codesquad.bookkbookk.common.jwt.JwtProvider;
 
 import lombok.NonNull;
@@ -18,19 +20,30 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
-public class TokenArgumentResolver implements HandlerMethodArgumentResolver {
+public class RefreshTokenArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final JwtProvider jwtProvider;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(Token.class) && parameter.getParameterType().equals(String.class);
+        return parameter.hasParameterAnnotation(RefreshToken.class) && parameter.getParameterType().equals(String.class);
     }
 
     @Override
     public String resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   @NonNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        return jwtProvider.getToken(Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class)));
+        Cookie[] cookies = Objects.requireNonNull(webRequest.getNativeRequest(HttpServletRequest.class)).getCookies();
+
+        if (cookies == null) {
+            throw new RefreshTokenNotFoundException();
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refreshToken")) {
+                return cookie.getValue();
+            }
+        }
+        throw new RefreshTokenNotFoundException();
     }
 
 }
