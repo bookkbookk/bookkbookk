e99c662189029a0dd20b524df69eaeea53d4fa01
nag0124 <wlsdnjs0124@gmail.com>
2023-11-15 18:03:20 +0900

    feat: JwtFilter에 refreshToken 검증 로직 추가 #109
    - RefreshToken의 검증을 JwtFilter에서 하도록 변경했습니다.

--- be/src/main/java/codesquad/bookkbookk/common/filter/JwtFilter.java
+++ be/src/main/java/codesquad/bookkbookk/common/filter/JwtFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import codesquad.bookkbookk.common.error.ErrorResponse;
 import codesquad.bookkbookk.common.error.exception.ApiException;
+import codesquad.bookkbookk.common.error.exception.RefreshTokenNotFoundException;
 import codesquad.bookkbookk.common.error.exception.jwt.MalformedTokenException;
 import codesquad.bookkbookk.common.error.exception.jwt.NoAuthorizationHeaderException;
 import codesquad.bookkbookk.common.error.exception.jwt.BearerPrefixNotIncludedException;
@@ -38,7 +40,7 @@ import lombok.extern.slf4j.Slf4j;
 public class JwtFilter implements Filter {
 
     private final String[] getUrlWhiteList = new String[]{};
-    private final String[] postUrlWhiteList = new String[]{"/api/auth/login*", "/api/auth/reissue"};
+    private final String[] postUrlWhiteList = new String[]{"/api/auth/login*"};
     private final JwtProvider jwtProvider;
     private final ObjectMapper objectMapper;
 
@@ -47,37 +49,78 @@ public class JwtFilter implements Filter {
             IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        boolean isTokenValid = false;
 
         if (checkGetWhiteList(httpServletRequest) || checkLoginWhiteListLogin(httpServletRequest)) {
             chain.doFilter(request, response);
             return;
         }
 
+        if (httpServletRequest.getRequestURI().equals("/api/auth/reissue")) {
+            isTokenValid = validateRefreshToken(httpServletRequest, httpServletResponse);
+        } else isTokenValid = validateAccessToken(httpServletRequest, httpServletResponse);
+
+        if (isTokenValid) {
+            chain.doFilter(request, response);
+        }
+    }
+
+    private boolean validateAccessToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
         String authorization = httpServletRequest.getHeader("Authorization");
+
         if (authorization == null) {
             writeErrorResponse(new NoAuthorizationHeaderException(), httpServletResponse);
-            return;
+            return false;
         }
         if (!authorization.startsWith("Bearer ")) {
             writeErrorResponse(new BearerPrefixNotIncludedException(), httpServletResponse);
-            return;
+            return false;
         }
 
-        String token = authorization.substring("Bearer ".length());
+        String accessToken = authorization.substring("Bearer ".length());
         try {
-            jwtProvider.validateToken(token);
+            jwtProvider.validateToken(accessToken);
         } catch (IllegalArgumentException e) {
             writeErrorResponse(new TokenNotIncludedException(), httpServletResponse);
-            return;
+            return false;
         } catch (MalformedJwtException | SecurityException e) {
             writeErrorResponse(new MalformedTokenException(), httpServletResponse);
-            return;
+            return false;
         } catch (ExpiredJwtException e) {
             writeErrorResponse(new TokenExpiredException(), httpServletResponse);
-            return;
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateRefreshToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        Cookie[] cookies = httpServletRequest.getCookies();
+        String refreshToken = null;
+
+        if (cookies == null) {
+            writeErrorResponse(new RefreshTokenNotFoundException(), httpServletResponse);
+            return false;
+        }
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("refreshToken")) {
+                refreshToken = cookie.getValue();
+            }
         }
 
-        chain.doFilter(request, response);
+        try {
+            jwtProvider.validateToken(refreshToken);
+        } catch (IllegalArgumentException e) {
+            writeErrorResponse(new TokenNotIncludedException(), httpServletResponse);
+            return false;
+        } catch (MalformedJwtException | SecurityException e) {
+            writeErrorResponse(new MalformedTokenException(), httpServletResponse);
+            return false;
+        } catch (ExpiredJwtException e) {
+            writeErrorResponse(new TokenExpiredException(), httpServletResponse);
+            return false;
+        }
+        return true;
     }
 
     private boolean checkGetWhiteList(HttpServletRequest httpServletRequest) {
