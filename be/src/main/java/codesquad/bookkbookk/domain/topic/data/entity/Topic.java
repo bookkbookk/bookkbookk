01a71e6e251adf69c6a7a352849aab62cb9e77c7
nag0124 <wlsdnjs0124@gmail.com>
2023-11-26 15:34:11 +0900

    feat: topic의 필드에 bookmark list 추가 #117

--- be/src/main/java/codesquad/bookkbookk/domain/topic/data/entity/Topic.java
+++ be/src/main/java/codesquad/bookkbookk/domain/topic/data/entity/Topic.java
@@ -1,5 +1,8 @@
 package codesquad.bookkbookk.domain.topic.data.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +10,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+import codesquad.bookkbookk.domain.bookmark.data.entity.Bookmark;
 import codesquad.bookkbookk.domain.chapter.data.entity.Chapter;
 
 import lombok.AccessLevel;
@@ -23,12 +28,13 @@ public class Topic {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "topic_id")
     private Long id;
-
     @ManyToOne
     @JoinColumn(name = "chapter_id")
     private Chapter chapter;
-
     private String title;
+
+    @OneToMany(mappedBy = "topic")
+    private List<Bookmark> bookmarks = new ArrayList<>();
 
     public Topic(Chapter chapter, String title) {
         this.chapter = chapter;
