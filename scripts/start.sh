4c7def16d472a33159282df59f9a191d05247582
nag0124 <wlsdnjs0124@gmail.com>
2024-02-21 15:56:13 +0900

    fix: 변수 지정시 스페이스 삭제

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT = "/home/ubuntu/bookkbookk"
-JAR_FILE = "$PROJECT_ROOT/app.jar"
+PROJECT_ROOT="/home/ubuntu/bookkbookk"
+JAR_FILE="$PROJECT_ROOT/app.jar"
 
-APP_LOG = "$PROJECT_ROOT/application.log"
-ERROR_LOG = "$PROJECT_ROOT/error.log"
-DEPLOY_LOG = "$PROJECT_ROOT/deploy.log"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/be/build/libs/*.jar $JAR_FILE
