5a0e579c9a67ffe39ad618ebe68c6d702b2af75b
nag0124 <wlsdnjs0124@gmail.com>
2024-02-28 20:21:52 +0900

    test: 테스트 코드 리팩토링 #161 (#162)
    - Dummy data 생성 메서드에 parameter로 number를 입력받아 data를 만들도록 변경
    - CreateBook 메서드의 creatorId 하드코딩 삭제

--- be/src/test/java/codesquad/bookkbookk/util/TestDataFactory.java
+++ be/src/test/java/codesquad/bookkbookk/util/TestDataFactory.java
@@ -19,68 +19,95 @@ public class TestDataFactory {
 
     public static Member createMember() {
         return Member.builder()
-                .email("nag@email.com")
+                .email("email@email.email")
                 .loginType(LoginType.GOOGLE)
-                .nickname("nag")
+                .nickname("nickname")
                 .profileImageUrl("profile")
                 .build();
     }
 
-    public static Member createAnotherMember() {
-        return Member.builder()
-                .email("gamgyul@email.com")
-                .loginType(LoginType.GOOGLE)
-                .nickname("gamgyul")
-                .profileImageUrl("profile")
-                .build();
+    public static List<Member> createMembers(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> Member.builder()
+                        .email("email@email.email " + number)
+                        .loginType(LoginType.GOOGLE)
+                        .nickname("nickname " + number)
+                        .profileImageUrl("profile" + number)
+                        .build())
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    public static BookClub createBookClub() {
+    public static BookClub createBookClub(Member creator) {
         return BookClub.builder()
-                .creatorId(1L)
-                .name("Test Book Club")
+                .creatorId(creator.getId())
+                .name("name")
                 .profileImageUrl("image.url")
                 .build();
     }
 
-    public static Book createBook1(BookClub bookClub) {
+    public static List<BookClub> createBookClubs(int count, Member creator) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> BookClub.builder()
+                        .creatorId(creator.getId())
+                        .name("name " + number)
+                        .profileImageUrl("image.url " + number)
+                        .build())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static Book createBook(BookClub bookClub) {
         return Book.builder()
-                .title("신데렐라")
+                .title("title")
                 .bookClub(bookClub)
-                .author("감귤")
-                .category("미스테리")
-                .cover("Cinderella")
+                .author("author")
+                .category("category")
+                .cover("cover")
                 .isbn("9791169210607")
                 .build();
-
     }
 
-    public static Book createBook2(BookClub bookClub) {
+    public static Book createBook(int number, BookClub bookClub) {
         return Book.builder()
-                .title("개똥벌레")
+                .title("title " + number)
                 .bookClub(bookClub)
-                .author("나그")
-                .category("스릴러")
-                .cover("gaeddong")
-                .isbn("1231231231232")
+                .author("author " + number)
+                .category("category " + number)
+                .cover("cover " + number)
+                .isbn("9791169210607")
                 .build();
-
     }
 
-    public static Chapter createChapter1(Book book) {
-        return new Chapter(book, "chapter 1");
+    public static List<Book> createBooks(int count, BookClub bookClub) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> Book.builder()
+                        .title("title " + number)
+                        .bookClub(bookClub)
+                        .author("author " + number)
+                        .category("category " + number)
+                        .cover("cover " + number)
+                        .isbn("9791169210607")
+                        .build())
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    public static Chapter createChapter2(Book book) {
-        return new Chapter(book, "chapter 2");
+    public static Chapter createChapter(Book book) {
+        return new Chapter(book, "chapter");
     }
 
-    public static Topic createTopic1(Chapter chapter) {
-        return new Topic(chapter, "topic 1");
+    public static List<Chapter> createChapters(int count, Book book) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> new Chapter(book, "chapter " + number))
+                .collect(Collectors.toUnmodifiableList());
     }
 
-    public static Topic createTopic2(Chapter chapter) {
-        return new Topic(chapter, "topic 2");
+    public static Topic createTopic(Chapter chapter) {
+        return new Topic(chapter, "topic");
+    }
+
+    public static List<Topic> createTopics(int count, Chapter chapter) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> new Topic(chapter, "topic " + number))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public static Bookmark createBookmark(Member writer, Topic topic) {
@@ -88,39 +115,43 @@ public class TestDataFactory {
                 .writer(writer)
                 .topic(topic)
                 .title("title")
-                .contents("content")
+                .contents("contents")
                 .build();
     }
 
-    public static Comment createComment(Bookmark bookmark, Member writer) {
-        return new Comment(bookmark, writer, "content");
-    }
-
-    public static Comment createAnotherComment(Bookmark bookmark, Member writer) {
-        return new Comment(bookmark, writer, "content 2");
-    }
-
-    public static Gathering createGathering(Book book) {
-        return new Gathering(book, LocalDateTime.of(2023, 12, 25, 13, 30),
-                "코드 스쿼드");
-    }
-
     public static List<Bookmark> createBookmarks(int count, Member writer, Topic topic) {
-        return IntStream.range(1, count + 1)
-                .mapToObj(index -> Bookmark.builder()
+        return IntStream.range(0, count)
+                .mapToObj(number -> Bookmark.builder()
                         .writer(writer)
                         .topic(topic)
-                        .title("title" + index)
-                        .contents("content" + index)
+                        .title("title " + number)
+                        .contents("contents " + number)
                         .build())
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    public static BookClub createDummyBookClub(int count) {
-        return BookClub.builder()
-                .creatorId(1L)
-                .name("Test Book Club" + count)
-                .profileImageUrl("image.url")
-                .build();
+    public static Comment createComment(Bookmark bookmark, Member writer) {
+        return new Comment(bookmark, writer, "contents");
     }
+
+    public static List<Comment> createComments(int count, Bookmark bookmark, Member writer) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> new Comment(bookmark, writer, "contents " + number))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static Gathering createGathering(Book book) {
+        return new Gathering(book, LocalDateTime.of(2023, 12, 25, 13, 30),
+                "place");
+    }
+
+    public static List<Gathering> createGatherings(int count, Book book) {
+        return IntStream.range(0, count)
+                .mapToObj(number -> new Gathering(book, LocalDateTime.of(2023, 12, 25, 13, 30),
+                        "place " + number))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+
+
 }
