37a9a2f945ed0c72f03a5356b7c0f89d08d7885b
nag0124 <wlsdnjs0124@gmail.com>
2024-03-08 19:00:29 +0900

    test: invitation code 저장, 만료 테스트 작성 #180

--- be/src/test/java/codesquad/bookkbookk/redis/RedisServiceTest.java
+++ be/src/test/java/codesquad/bookkbookk/redis/RedisServiceTest.java
@@ -2,14 +2,19 @@ package codesquad.bookkbookk.redis;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import codesquad.bookkbookk.IntegrationTest;
 import codesquad.bookkbookk.common.jwt.JwtProperties;
 import codesquad.bookkbookk.common.jwt.JwtProvider;
 import codesquad.bookkbookk.common.redis.RedisService;
+import codesquad.bookkbookk.domain.bookclub.data.entity.BookClub;
+import codesquad.bookkbookk.domain.bookclub.repository.BookClubRepository;
 import codesquad.bookkbookk.domain.member.data.entity.Member;
 import codesquad.bookkbookk.domain.member.repository.MemberRepository;
 import codesquad.bookkbookk.util.TestDataFactory;
@@ -27,6 +32,12 @@ public class RedisServiceTest extends IntegrationTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private BookClubRepository bookClubRepository;
+
+    @Value("${invitation.expiration}")
+    private long invitationCodeExpiration;
 
     @Test
     @DisplayName("access token을 redis에 저장한다.")
@@ -63,7 +74,7 @@ public class RedisServiceTest extends IntegrationTest {
 
     @Test
     @DisplayName("redis에 refreshToken을 저장한다.")
-    void saveRefreshToken() throws InterruptedException {
+    void saveRefreshToken() {
         // given
         Member member = TestDataFactory.createMember();
         memberRepository.save(member);
@@ -92,6 +103,45 @@ public class RedisServiceTest extends IntegrationTest {
 
         // then
         assertThat(redisService.getMemberIdByRefreshToken(refreshToken)).isNull();
+    }
+
+    @Test
+    @DisplayName("redis에 invitation code를 저장한다.")
+    void saveInvitationCode() {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+
+        BookClub bookClub = TestDataFactory.createBookClub(member);
+        bookClubRepository.save(bookClub);
+
+        String invitationCode = String.valueOf(UUID.randomUUID());
+
+        // when
+        redisService.saveRefreshToken(invitationCode, bookClub.getId());
+
+        // then
+        assertThat(redisService.getMemberIdByRefreshToken(invitationCode)).isEqualTo(bookClub.getId());
+    }
+
+    @Test
+    @DisplayName("redis에 저장된 invitation code가 만료된 뒤 그 invitation code를 redis에 조회하면 존재하지 않는다.")
+    void expireSavedInvitationCode() throws InterruptedException {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+
+        BookClub bookClub = TestDataFactory.createBookClub(member);
+        bookClubRepository.save(bookClub);
+
+        String invitationCode = String.valueOf(UUID.randomUUID());
+
+        // when
+        redisService.saveRefreshToken(invitationCode, bookClub.getId());
+        Thread.sleep(invitationCodeExpiration + 1000);
+
+        // then
+        assertThat(redisService.getBookClubIdByInvitationCode(invitationCode)).isNull();
     }
 
 }
