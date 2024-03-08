69163145a63bbd7818e025e90e2e78374e7c23f1
nag0124 <wlsdnjs0124@gmail.com>
2024-03-08 10:41:40 +0900

    test: access token 저장, 확인 기능 테스트 작성 #180
    - 토큰이 저장되는 것을 확인합니다.
    - 만료 시간이 지나면 redis에 없음을 확인합니다.
    
    - prefix를 사용하여 key를 만들고 레디스에 넣어줬습니다.
    - expiration을 지정하여 일정 시간 이후 만료되도록 설정했습니다.
    - @Transactional에서 readOnly 옵션이 없기에 메소드가 아닌 클래스에 어노테이션을 붙였습니다.

---
+++ be/src/test/java/codesquad/bookkbookk/redis/RedisServiceTest.java
@@ -0,0 +1,64 @@
+package codesquad.bookkbookk.redis;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import codesquad.bookkbookk.IntegrationTest;
+import codesquad.bookkbookk.common.jwt.JwtProperties;
+import codesquad.bookkbookk.common.jwt.JwtProvider;
+import codesquad.bookkbookk.common.redis.RedisService;
+import codesquad.bookkbookk.domain.member.data.entity.Member;
+import codesquad.bookkbookk.domain.member.repository.MemberRepository;
+import codesquad.bookkbookk.util.TestDataFactory;
+
+public class RedisServiceTest extends IntegrationTest {
+
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("access token을 redis에 저장한다.")
+    void saveAccessToken() {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        // when
+        redisService.saveAccessToken(accessToken);
+
+        // then
+        assertThat(redisService.isAccessTokenPresent(accessToken)).isTrue();
+    }
+
+    @Test
+    @DisplayName("redis에 저장된 access toekn이 만료된 그 access token을 redis에 조회하면 존재하지 않는다.")
+    void expireSavedAccessToken() throws InterruptedException {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        // when
+        redisService.saveAccessToken(accessToken);
+        Thread.sleep(jwtProperties.getAccessTokenExpiration() + 1000);
+
+        // then
+        assertThat(redisService.isAccessTokenPresent(accessToken)).isFalse();
+    }
+
+}
