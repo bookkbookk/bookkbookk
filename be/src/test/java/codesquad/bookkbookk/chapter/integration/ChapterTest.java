d4fe539099c16d528ab0aafb4c88569b985a917f
nag0124 <wlsdnjs0124@gmail.com>
2024-05-02 20:13:06 +0900

    test: Chapter update 테스트 코드 추가 #219
    - Chatper를 업데이트할 때, statusId가 null이면 status를 변경하지 않는다.
    - Chatper를 업데이트할 때, title이 null이면 title을 변경하지 않는다.

--- be/src/test/java/codesquad/bookkbookk/chapter/integration/ChapterTest.java
+++ be/src/test/java/codesquad/bookkbookk/chapter/integration/ChapterTest.java
@@ -96,7 +96,7 @@ public class ChapterTest extends IntegrationTest {
 
     @DisplayName("챕터의 상태와 제목을 변경한다.")
     @Test
-    void updateChapterTitle() {
+    void updateChapter() {
         //given
         Member member = TestDataFactory.createMember();
         memberRepository.save(member);
@@ -132,6 +132,88 @@ public class ChapterTest extends IntegrationTest {
             softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
             softAssertions.assertThat(response.jsonPath().getString("title")).isEqualTo("update");
             softAssertions.assertThat(response.jsonPath().getString("statusId")).isEqualTo("2");
+        });
+    }
+
+    @DisplayName("Chatper를 업데이트할 때, statusId가 null이면 status를 변경하지 않는다.")
+    @Test
+    void updateChapterTitle() {
+        //given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        BookClub bookClub = TestDataFactory.createBookClub(member);
+        bookClubRepository.save(bookClub);
+
+        BookClubMember bookClubMember = new BookClubMember(bookClub, member);
+        bookClubMemberRepository.save(bookClubMember);
+
+        Book book = TestDataFactory.createBook(bookClub);
+        bookRepository.save(book);
+
+        Chapter chapter = TestDataFactory.createChapter(book);
+        chapterRepository.save(chapter);
+
+        JSONObject requestBody = new JSONObject(Map.of("title", "update"));
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .when()
+                .patch("/api/chapters/" + chapter.getId())
+                .then().log().all()
+                .extract();
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(response.jsonPath().getString("title")).isEqualTo("update");
+            softAssertions.assertThat(response.jsonPath().getInt("statusId")).isEqualTo(1);
+        });
+    }
+
+    @DisplayName("Chatper를 업데이트할 때, title이 null이면 title을 변경하지 않는다.")
+    @Test
+    void updateChapterStatus() {
+        //given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+        String accessToken = jwtProvider.createAccessToken(member.getId());
+
+        BookClub bookClub = TestDataFactory.createBookClub(member);
+        bookClubRepository.save(bookClub);
+
+        BookClubMember bookClubMember = new BookClubMember(bookClub, member);
+        bookClubMemberRepository.save(bookClubMember);
+
+        Book book = TestDataFactory.createBook(bookClub);
+        bookRepository.save(book);
+
+        Chapter chapter = TestDataFactory.createChapter(book);
+        chapterRepository.save(chapter);
+
+        JSONObject requestBody = new JSONObject(Map.of("statusId", 2));
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .contentType(ContentType.JSON)
+                .body(requestBody.toString())
+                .when()
+                .patch("/api/chapters/" + chapter.getId())
+                .then().log().all()
+                .extract();
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            softAssertions.assertThat(response.jsonPath().getString("title")).isEqualTo("chapter");
+            softAssertions.assertThat(response.jsonPath().getInt("statusId")).isEqualTo(2);
         });
     }
 
