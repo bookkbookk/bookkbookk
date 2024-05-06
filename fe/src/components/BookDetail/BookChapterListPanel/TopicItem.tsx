a8908522b1c5ccb0885615ca81deec17723d79f1
nag0124 <wlsdnjs0124@gmail.com>
2024-05-06 17:16:18 +0900

    feat: TopicItem의 RecentBookmark가 없는 경우 렌더링 추가 #212

--- fe/src/components/BookDetail/BookChapterListPanel/TopicItem.tsx
+++ fe/src/components/BookDetail/BookChapterListPanel/TopicItem.tsx
@@ -4,6 +4,7 @@ import { styled } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { TreeItem } from "@mui/x-tree-view";
+import NoUserImage from "@assets/images/brand.png";
 
 export function TopicItem({
   topic,
@@ -23,13 +24,24 @@ export function TopicItem({
       label={
         <Stack display={"flex"} flexDirection={"row"} gap={1}>
           <Content sx={{ width: "60%" }}>{title}</Content>
-          <Stack display={"flex"} flexDirection={"row"} gap={1} width={"60%"}>
-            <Avatar
-              src={recentBookmark.authorProfileImgUrl}
-              sx={{ width: 24, height: 24 }}
-            />
-            <Content>{recentBookmark.content}</Content>
-          </Stack>
+          { recentBookmark ? (
+            <Stack display={"flex"} flexDirection={"row"} gap={1} width={"60%"}>
+              <Avatar
+                src={recentBookmark.authorProfileImgUrl}
+                sx={{ width: 24, height: 24 }}
+              />
+              <Content>{recentBookmark.content}</Content>
+            </Stack>
+          ) : (
+            <Stack display={"flex"} flexDirection={"row"} gap={1} width={"60%"}>
+              <Avatar
+                src={NoUserImage}
+                sx={{ width: 24, height: 24 }}
+              />
+              <Content>"북마크가 없습니다."</Content>
+            </Stack>
+          )
+          }
         </Stack>
       }
     />
