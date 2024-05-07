5d996df1e689f2801a999d3b78f4470e0a2a15b8
nag0124 <wlsdnjs0124@gmail.com>
2024-05-07 12:06:25 +0900

    fix: CommentFooter와 CommentContent import 추가 #233

--- fe/src/components/common/Comment/index.tsx
+++ fe/src/components/common/Comment/index.tsx
@@ -3,12 +3,16 @@ import ButtonFooter from "./ButtonFooter";
 import CommentContainer from "./CommentContainer";
 import CommentEditor from "./CommentEditor";
 import CommentHeader from "./CommentHeader";
+import CommentContent from "./CommentContent";
+import CommentFooter from "./CommentFooter";
 import CommentViewer from "./CommentViewer";
 import PageEditor from "./PageEditor";
 import PageViewer from "./PageViewer";
 
 export const Comment = Object.assign(CommentContainer, {
   Header: CommentHeader,
+  Content: CommentContent,
+  Footer: CommentFooter,
   ContentViewer: CommentViewer,
   ContentEditor: CommentEditor,
   PageEditor: PageEditor,
