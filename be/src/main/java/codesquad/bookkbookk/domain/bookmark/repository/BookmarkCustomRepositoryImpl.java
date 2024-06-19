bd4115ebaee461e73f9f5fc6b3bfdb99624fe160
nag0124 <wlsdnjs0124@gmail.com>
2024-06-19 17:55:01 +0900

    feat: 동적 쿼리 사용하는 Repository 구현 #246
    - 여러 조건에 따라 `BooleanExpression`을 반환하도록 코드를 짰습니다.
    - `Page` 필터만 적용된 경우에만 페이지 오름차순으로 정렬하고 나머지 경우는 최신순으로 정렬합니다.

---
+++ be/src/main/java/codesquad/bookkbookk/domain/bookmark/repository/BookmarkCustomRepositoryImpl.java
@@ -0,0 +1,77 @@
+package codesquad.bookkbookk.domain.bookmark.repository;
+
+import static codesquad.bookkbookk.domain.book.data.entity.QBook.*;
+import static codesquad.bookkbookk.domain.bookmark.data.entity.QBookmark.*;
+import static codesquad.bookkbookk.domain.chapter.data.entity.QChapter.*;
+import static codesquad.bookkbookk.domain.member.data.entity.QMember.*;
+import static codesquad.bookkbookk.domain.topic.data.entity.QTopic.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import codesquad.bookkbookk.domain.bookmark.data.dto.BookmarkFilter;
+import codesquad.bookkbookk.domain.bookmark.data.entity.Bookmark;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class BookmarkCustomRepositoryImpl implements BookmarkCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Bookmark> findAllByFilter(Long bookId, BookmarkFilter bookmarkFilter) {
+        return jpaQueryFactory
+                .selectFrom(bookmark)
+                .innerJoin(bookmark.writer, member)
+                .fetchJoin()
+                .innerJoin(bookmark.topic, topic)
+                .innerJoin(topic.chapter, chapter)
+                .innerJoin(chapter.book, book)
+                .where(book.id.eq(bookId), createPageCondition(bookmarkFilter), createTimeCondition(bookmarkFilter))
+                .orderBy(createOrder(bookmarkFilter))
+                .fetch();
+    }
+
+    private BooleanExpression createPageCondition(BookmarkFilter bookmarkFilter) {
+        if (bookmarkFilter.getStartTime() != null && bookmarkFilter.getEndTime() != null) {
+            return bookmark.updatedTime.between(bookmarkFilter.getStartTime(), bookmarkFilter.getEndTime());
+        }
+        if (bookmarkFilter.getStartTime() != null) {
+            return bookmark.updatedTime.goe(bookmarkFilter.getStartTime());
+        }
+        if (bookmarkFilter.getEndTime() != null) {
+            return bookmark.updatedTime.loe(bookmarkFilter.getEndTime());
+        }
+        return null;
+    }
+
+    private BooleanExpression createTimeCondition(BookmarkFilter bookmarkFilter) {
+        if (bookmarkFilter.getStartPage() != null && bookmarkFilter.getEndPage() != null) {
+            return bookmark.page.between(bookmarkFilter.getStartPage(), bookmarkFilter.getEndPage());
+        }
+        if (bookmarkFilter.getStartPage() != null) {
+            return bookmark.page.goe(bookmarkFilter.getStartPage());
+        }
+        if (bookmarkFilter.getEndPage() != null) {
+            return bookmark.page.loe(bookmarkFilter.getEndPage());
+        }
+        return null;
+    }
+
+    private OrderSpecifier createOrder(BookmarkFilter bookmarkFilter) {
+        if (bookmarkFilter.getStartTime() == null && bookmarkFilter.getEndTime() == null &&
+                (bookmarkFilter.getStartPage() != null || bookmarkFilter.getEndPage() != null)) {
+            return bookmark.page.asc();
+        } else {
+            return bookmark.updatedTime.desc();
+        }
+    }
+
+}
