a298b8fce2408cf5d3c6adce791c1fa8552786e9
nag0124 <wlsdnjs0124@gmail.com>
2024-07-20 16:44:58 +0900

    feat: `Chapter` batch insert 기능 구현 #261
    - `NamedParameterJdbcTemplate`의 `updateBatch()` 메서드를 사용하여 구현했습니다.
    - `LAST_INSERT_ID()` 쿼리를 이용하여 아이디를 넣어줍니다.

--- be/src/main/java/codesquad/bookkbookk/domain/chapter/repository/ChapterCustomRepositoryImpl.java
+++ be/src/main/java/codesquad/bookkbookk/domain/chapter/repository/ChapterCustomRepositoryImpl.java
@@ -4,13 +4,18 @@ import static codesquad.bookkbookk.domain.bookmark.data.entity.QBookmark.*;
 import static codesquad.bookkbookk.domain.chapter.data.entity.QChapter.*;
 import static codesquad.bookkbookk.domain.topic.data.entity.QTopic.*;
 
+import java.sql.Types;
 import java.util.List;
 
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import codesquad.bookkbookk.common.error.exception.LastInsertIdDoesNotExistException;
 import codesquad.bookkbookk.common.type.Status;
 import codesquad.bookkbookk.domain.chapter.data.entity.Chapter;
 
@@ -20,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChapterCustomRepositoryImpl implements ChapterCustomRepository {
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final JPAQueryFactory jpaQueryFactory;
 
     // TODO: 쿼리 성능 개선하기
@@ -36,11 +42,52 @@ public class ChapterCustomRepositoryImpl implements ChapterCustomRepository {
                 .fetch();
     }
 
+    @Override
+    public List<Chapter> saveAllInBulk(List<Chapter> chapters) {
+        String sql = "INSERT INTO chapter (book_id, status, title) VALUES (:book_id, :status, :title)";
+        SqlParameterSource[] batchParameters = createBatchParameters(chapters);
+
+        namedParameterJdbcTemplate.batchUpdate(sql, batchParameters);
+        setChapterIds(chapters);
+        return chapters;
+    }
+
     private BooleanExpression createChapterStatusCondition(Status status) {
         if (status == Status.ALL) {
             return null;
         }
         return chapter.status.eq(status);
+    }
+
+
+    private SqlParameterSource[] createBatchParameters(List<Chapter> chapters) {
+        SqlParameterSource[] batchParameters = new SqlParameterSource[chapters.size()];
+
+        for (int i = 0; i < chapters.size(); i++) {
+            Chapter chapter = chapters.get(i);
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+
+            parameters.addValue("book_id", chapter.getBookId(), Types.BIGINT);
+            parameters.addValue("status", chapter.getStatus(), Types.VARCHAR);
+            parameters.addValue("title", chapter.getTitle(), Types.VARCHAR);
+            batchParameters[i] = parameters;
+        }
+        return batchParameters;
+    }
+
+    private void setChapterIds(List<Chapter> chapters) {
+        Long startId = namedParameterJdbcTemplate.getJdbcTemplate().queryForObject(
+                "SELECT LAST_INSERT_ID()", (rs, rowNum) -> rs.getLong("LAST_INSERT_ID()")
+        );
+
+        if (startId == null) {
+            throw new LastInsertIdDoesNotExistException();
+        }
+
+        for (int i = 0; i < chapters.size(); i++) {
+            chapters.get(i).setId(startId + i);
+        }
+
     }
 
 }
---
+++ be/src/test/java/codesquad/bookkbookk/unit/ChapterCustomRepositoryImplTest.java
@@ -0,0 +1,68 @@
+package codesquad.bookkbookk.unit;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import codesquad.bookkbookk.common.config.QueryDslConfig;
+import codesquad.bookkbookk.domain.book.data.entity.Book;
+import codesquad.bookkbookk.domain.book.repository.BookRepository;
+import codesquad.bookkbookk.domain.bookclub.data.entity.BookClub;
+import codesquad.bookkbookk.domain.bookclub.repository.BookClubRepository;
+import codesquad.bookkbookk.domain.chapter.data.entity.Chapter;
+import codesquad.bookkbookk.domain.chapter.repository.ChapterRepository;
+import codesquad.bookkbookk.domain.member.data.entity.Member;
+import codesquad.bookkbookk.domain.member.repository.MemberRepository;
+import codesquad.bookkbookk.util.TestDataFactory;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(QueryDslConfig.class)
+public class ChapterCustomRepositoryImplTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private BookClubRepository bookClubRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private ChapterRepository chapterRepository;
+
+    @Test
+    @DisplayName("Chapters를 Batch Insert한다.")
+    void saveChaptersInBatch() {
+        // given
+        Member member = TestDataFactory.createMember();
+        memberRepository.save(member);
+
+        BookClub bookClub = TestDataFactory.createBookClub(member);
+        bookClubRepository.save(bookClub);
+
+        Book book = TestDataFactory.createBook(bookClub);
+        bookRepository.save(book);
+
+        List<Chapter> chapters = TestDataFactory.createChapters(100, book);
+
+        // when
+        chapterRepository.saveAllInBatch(chapters);
+
+        // then
+        chapters.forEach(
+                chapter -> assertThat(chapter.getId()).isNotNull()
+        );
+    }
+
+}
